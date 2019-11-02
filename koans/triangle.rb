cbd516f8f9d2b56f2160d0baf42ea0d206704e9d
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-11-02 13:11:23 +0200

    Triangle 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError, "Sides must by numbers greater than zero" if (a <= 0) || (b <= 0) || (c <= 0)
+  raise TriangleError, "No two sides can add to be less than or equal to the other side" if (a + b <= c) || (a + c <= b) || (b + c <= a)
   if a == b && a == c
     :equilateral
   elsif a == b || a == c || c == b
